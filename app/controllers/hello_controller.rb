9275868daa4ae7d128b0252018b3be0edbe9ede7
xlisyuan <xlisyuan@gmail.com>
2020-07-09 00:35:21 +0800

    request & response

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,2 +1,31 @@
 class HelloController < ApplicationController
+	def actionEat
+		render plain: "O3O"
+	end
+
+	def request_headers
+		render plain: request.headers.to_h.reject{ |key, value|
+		  key.include? '.'
+		}.map{ |key, value|
+		  "#{key}: #{value}"
+		}.sort.join("\n")
+	end
+
+	def request_body
+		render plain: request.body
+	end
+
+	def response_headers
+		response.headers['plus'] = 'O v O'
+		render plain: response.headers.to_h.map{ |key, value|
+		  "#{key}: #{value}"
+		}.sort.join("\n")
+	  end
+
+	  def show_response_body
+		puts "===這是設定前的response.body:#{response.body}==="
+		render plain: "clap"
+		puts "===這是設定後的response.body:#{response.body}==="
+		#render plain: response.body
+	  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/hello/eat', to: 'hello#actionEat'
+  get '/hello/request_headers', to: 'hello#request_headers'
+  get '/hello/request_body', to: 'hello#request_body'
+  get '/hello/response_headers', to: 'hello#response_headers'
+  get '/hello/response_body', to: 'hello#show_response_body'
 end
+#http://localhost:3000/hello/eat
+#http://localhost:3000/hello/request_headers
+#http://localhost:3000/hello/request_body
+#http://localhost:3000/hello/response_headers
+#http://localhost:3000/hello/response_body
